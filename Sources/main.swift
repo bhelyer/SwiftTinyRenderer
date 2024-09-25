0223a9f1919f6f620016b8fb05f0dbd60ab2e3f0
Bernard Helyer <b.helyer@gmail.com>
2024-09-26 07:47:50 +1200

    Lesson 2, initial triangle rasterisation.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,29 @@
 import Foundation
 
+let white = TGAColour(r: 255, g: 255, b: 255, a: 255)
+let red = TGAColour(r: 255, g: 0, b: 0, a: 255)
+let green = TGAColour(r: 0, g: 255, b: 0, a: 255)
+let width = 800
+let height = 800
+
+var tImage = TGAImage(width: 200, height: 200, format: .rgb)
+let t0 = [Vec2i(x: 10, y: 70), Vec2i(x: 50, y: 160), Vec2i(x: 70, y: 80)]
+let t1 = [Vec2i(x: 180, y: 50), Vec2(x: 150, y: 1), Vec2i(x: 70, y: 180)]
+let t2 = [Vec2i(x: 180, y: 150), Vec2i(x: 120, y: 160), Vec2i(x: 130, y: 180)]
+triangle(t0[0], t0[1], t0[2], &tImage, red)
+triangle(t1[0], t1[1], t1[2], &tImage, white)
+triangle(t2[0], t2[1], t2[2], &tImage, green)
+tImage.flipVertically() // Move origin to bottom left corner.
+if !tImage.write(fileTo: "triangles.tga", vflip: false, rle: true) {
+    print("Failed to write image.")
+}
+
 if CommandLine.arguments.count != 2 {
     print("usage: tinyrenderer model_file.obj")
     exit(1)
 }
 let model = Model(fromFile: CommandLine.arguments[1])
 
-let white = TGAColour(r: 255, g: 255, b: 255, a: 255)
-let red = TGAColour(r: 255, g: 0, b: 0, a: 255)
-let width = 800
-let height = 800
 
 var image = TGAImage(width: width, height: height, format: .rgb)
 
@@ -49,6 +63,61 @@ func line(_ x0: Int, _ y0: Int, _ x1: Int, _ y1: Int, _ image: inout TGAImage, _
         if error > dx {
             y += y1 > y0 ? 1 : -1
             error -= dx * 2
+        }
+    }
+}
+
+func line(_ a: Vec2i, _ b: Vec2i, _ image: inout TGAImage, _ colour: TGAColour) {
+    line(a.x, a.y, b.x, b.y, &image, colour)
+}
+
+@MainActor
+func triangle(_ v0: Vec2i, _ v1: Vec2i, _ v2: Vec2i, _ image: inout TGAImage, _ colour: TGAColour) {
+    // Create mutable copies of the input parameters.
+    var v0 = v0
+    var v1 = v1
+    var v2 = v2
+
+    // Sort the input paramaters in ascending Y order.
+    if v0.y > v1.y { swap(&v0, &v1) }
+    if v0.y > v2.y { swap(&v0, &v2) }
+    if v1.y > v2.y { swap(&v1, &v2) }
+
+    // Draw the bottom half of the triangle.
+    // The height of the triangle.
+    let totalHeight = v2.y - v0.y
+    for y in v0.y...v1.y {
+        // The height of the bottom half.
+        let segmentHeight = v1.y - v0.y + 1
+        // 0 = bottom of triangle, 1 = top of triangle
+        // how far along through the long line we are
+        let alpha = Real(y - v0.y) / Real(totalHeight)
+        // 0 = bottom of segment, 1 = top of segment
+        // how far along through the short line we are
+        let beta  = Real(y - v0.y) / Real(segmentHeight)
+        // (v2 - v0) * alpha -- scale v0 from v2 by alpha
+        // +v0 -- put it back in right coordinate system
+        var a = v0 + (v2-v0) * alpha
+        // ditto for v0 to v1, scaled by beta.
+        var b = v0 + (v1-v0) * beta
+        // a is the point along the long line
+        // b is the point along the short line
+        if a.x > b.x { swap(&a, &b) }
+        for j in a.x...b.x {
+            image.set(x: j, y: y, to: colour)
+        }
+    }
+    // Do the same for the top half.
+    // (Except the beta line goes from v1 to v2.)
+    for y in v1.y...v2.y {
+        let segmentHeight = v2.y - v1.y + 1
+        let alpha = Real(y - v0.y) / Real(totalHeight)
+        let beta = Real(y - v1.y) / Real(segmentHeight)
+        var a = v0 + (v2-v0) * alpha
+        var b = v1 + (v2-v1) * beta
+        if a.x > b.x { swap(&a, &b) }
+        for j in a.x...b.x {
+            image.set(x: j, y: y, to: colour)
         }
     }
 }
