a1f1a5e26312e5b4c0709cefa102e5028d7852b0
Bernard Helyer <b.helyer@gmail.com>
2024-10-05 18:42:29 +1300

    Rename tinyrenderer target to TinyRenderer.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "tinyrenderer",
+    name: "TinyRenderer",
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "tinyrenderer")
+            name: "TinyRenderer"),
+        //.testTarget(name: "TinyRendererTests", dependencies: ["TinyRenderer"])
     ]
 )
--- Sources/TinyRenderer/Model.swift
+++ Sources/TinyRenderer/Model.swift
@@ -2,26 +2,32 @@ import Foundation
 
 private let vertPrefix = "v "
 private let facePrefix = "f "
+private let texVertPrefix = "vt  "
 
 /// An exceedingly simple WaveFront OBJ 3D model parser.
 public struct Model {
     public struct Face {
-        public let vertIndices: [Int]
+        public var vertIndices: [Int]
+        public var texVertIndices: [Int]
         
         public init() {
             self.vertIndices = []
+            self.texVertIndices = []
         }
         
         public init(vertIndices: [Int]) {
             self.vertIndices = vertIndices
+            self.texVertIndices = [] // !
         }
     }
     
     private let verts: [Vec3r]
+    private let texVerts: [Vec3r]
     private let faces: [Face]
 
     public init(fromFile filename: String) {
         var verts: [Vec3r] = []
+        var texVerts: [Vec3r] = []
         var faces: [Face] = []
 
         var modelText: String
@@ -30,6 +36,7 @@ public struct Model {
         } catch {
             print("Failed to open file \"\(filename)\" for reading.")
             self.verts = []
+            self.texVerts = []
             self.faces = []
             return
         }
@@ -40,11 +47,14 @@ public struct Model {
                 verts.append(parseVert(line: line))
             } else if line.hasPrefix(facePrefix) {
                 faces.append(parseFace(line: line))
+            } else if line.hasPrefix(texVertPrefix) {
+                texVerts.append(parseTextureVert(line: line))
             }
         }
         self.verts = verts
+        self.texVerts = texVerts
         self.faces = faces
-        print("# #v \(verts.count) #f \(faces.count)")
+        print("# #v \(verts.count) #tv \(texVerts.count) #f \(faces.count)")
     }
 
     public var nverts: Int {
@@ -86,6 +96,28 @@ private func parseVert(line: Substring) -> Vec3r {
     return Vec3r(verts[0]!, verts[1]!, verts[2]!)
 }
 
+private func parseTextureVert(line: Substring) -> Vec3r {
+    if line.count <= texVertPrefix.count {
+        print("Invalid vert line given to parseTextureVert.")
+        return Vec3r()
+    }
+    let startIndex = line.index(line.startIndex, offsetBy: texVertPrefix.count)
+    let vertsStr = line[startIndex..<line.endIndex]
+    let vertStrs = vertsStr.split { $0 == " " }
+    let verts = vertStrs.map { Real($0) }
+    if verts.count != 3 {
+        print("More then 3 coordinates on a vert line.")
+        return Vec3r()
+    }
+    for vert in verts {
+        if vert == nil {
+            print("Failed to parse a coordinate on a vert line.")
+            return Vec3r()
+        }
+    }
+    return Vec3r(verts[0]!, verts[1]!, verts[2]!)
+}
+
 private func parseFace(line: Substring) -> Model.Face {
     if line.count <= facePrefix.count {
         print("Invalid face line given to parseFace.")
@@ -97,7 +129,7 @@ private func parseFace(line: Substring) -> Model.Face {
     if faceGroups.count == 0 {
         return Model.Face()
     }
-    var faces: [Int] = []
+    var face = Model.Face()
     // Given "f 1/2/3 4/5/6 7/8/9", create an array [1-1, 4-1, 7-1].
     for faceGroup in faceGroups {
         let faceStrs = faceGroup.split { $0 == "/" }
@@ -108,7 +140,12 @@ private func parseFace(line: Substring) -> Model.Face {
             return Model.Face()
         }
         // In OBJ files, the first index is 1, not 0.
-        faces.append(faceIndex - 1)
+        face.vertIndices.append(faceIndex - 1)
+        if faceStrs.count > 1 {
+            if let faceTexIndex = Int(faceStrs[1]) {
+                face.texVertIndices.append(faceTexIndex - 1)
+            }
+        }
     }
-    return Model.Face(vertIndices: faces)
+    return face
 }
